864b11275dc261ec4baa361afe5a1698598211ba
Adriana <adrianaordonez8616@gmail.com>
2023-06-13 22:38:04 -0400

    edited index.html and index.js

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const today = new Date();
+const thisYear = today.getFullYear();
+const footer = document.querySelector('footer');
+const copyright = document.createElement('p');
+copyright.innerHTML = 'Adriana ' + '\u00A9'+ thisYear;
+footer.appendChild(copyright);
+
+const skills = ["JavaScript","HTML", "CSS"];
+const skillsSection = document.getElementById('skills');
+const skillsList = skillsSection.querySelector('ul');
+for (let i=0; i < skills.length; i++){
+    const skill = document.createElement('li');
+    skill.innerText = skills[i];
+    skillsList.appendChild(skill);
+}
